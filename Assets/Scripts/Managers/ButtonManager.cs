afcda116459dc28c6c8865ccb49a26df8e398e6c
7433jun <7433jun@naver.com>
2023-11-11 20:34:38 +0900

    (2023.11.11) 대충

---
+++ Assets/Scripts/Managers/ButtonManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void SetActiveButton(GameObject gameObject)
+    {
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
